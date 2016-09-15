57aeb6b0b6a806ae22fa87b62b420c1eb994f0d3
Eli Sadoff <snood1205@gmail.com>
2016-09-15 13:24:14 -0400

    string interpolation

--- say_goodnight.rb
+++ say_goodnight.rb
@@ -1,8 +1,5 @@
 def say_goodnight(name)
-  result = "Good night, " + name
+  result = "Good night, #{name}" 
   return result
 end
-
-# Time for bed...
-puts say_goodnight("John-Boy")
-puts say_goodnight("Mary-Ellen")
+puts say_goodnight('Pa')
