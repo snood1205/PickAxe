36df8fa4642096116e085027597a3a8395d6eafd
Eli Sadoff <snood1205@gmail.com>
2016-09-15 13:38:56 -0400

    !=>

--- symbols.rb
+++ symbols.rb
@@ -13,3 +13,14 @@ p inst_section[:cello]
 # Note that strings aren't the same as symbols...
 p inst_section['cello']
 
+# Alternate (non-hashrocket) hash/symbol notation
+
+inst_section = {
+  cello:    'string',
+  clarinet: 'woodwind',
+  drum:     'percussion',
+  oboe:     'woodwind',
+  trumpet:  'brass',
+  violin:   'string'
+}
+puts "An oboe is a #{inst_section[:oboe]} instrument"
