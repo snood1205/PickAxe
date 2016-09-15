f59c9e765d5fb968d0d1db20f418e893040234b7
Eli Sadoff <snood1205@gmail.com>
2016-09-15 13:32:43 -0400

    arrays and hashes

---
+++ arrays_and_hashes.rb
@@ -0,0 +1,28 @@
+a = [ 1, 'cat', 3.14 ] # array with three elements
+puts "The first element is #{a[0]}"
+# set the third element
+a[2] = nil
+puts "The array is now #{a.inspect}"
+
+a = [ 'ant', 'bee', 'cat', 'dog', 'elk' ]
+p a[0]
+p a[3]
+# this is the same:
+a = %w{ ant bee cat dog elk }
+p a[0]
+p a[3]
+
+# Hashes
+
+inst_section = {
+  'cello'     => 'string',
+  'clarinet'  => 'woodwind',
+  'drum'      => 'percussion',
+  'oboe'      => 'woodwind',
+  'trumpet'   => 'brass',
+  'violin'    => 'string'
+}
+
+p inst_section['oboe']
+p inst_section['cello']
+p inst_section['bassoon']
