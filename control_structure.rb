54a8e845304c9ed7cf2e751a19af4e3ed1bdd33e
Eli Sadoff <snood1205@gmail.com>
2016-09-15 13:41:43 -0400

    While loop

--- control_structure.rb
+++ control_structure.rb
@@ -1,3 +1,4 @@
+# If-Elsif-Else
 today = Time.now
 
 if today.saturday?
@@ -6,4 +7,14 @@ elsif today.sunday?
   puts "Relax"
 else
   puts "Go to work"
+end
+
+# While
+
+num_pallets = 0
+weight      = 0
+while weight < 100 and num_pallets <= 5
+  pallet  = next_pallet()
+  weight += pallet.weight
+  num_pallets += 1
 end
